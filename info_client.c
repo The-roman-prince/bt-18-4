45b2f53087ecd62fd3aee81bf73462343915295b
The-roman-prince <121484059+The-roman-prince@users.noreply.github.com>
2023-04-24 20:30:15 +0700

    lastest update

--- info_client.c
+++ info_client.c
@@ -1,44 +1,77 @@
 #include <stdio.h>
-#include <string.h>
-#include <sys/socket.h>
 #include <sys/types.h>
-#include <netdb.h>
-#include <arpa/inet.h>
+#include <sys/socket.h>
 #include <unistd.h>
-
+#include <netdb.h>
+#include <string.h>
+#include <arpa/inet.h>
+#define MAX_SIZE 128
 int main()
 {
+    // Khai bao socket
     int client = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-
+    // Khai bao dia chi cua server
     struct sockaddr_in addr;
     addr.sin_family = AF_INET;
     addr.sin_addr.s_addr = inet_addr("127.0.0.1");
     addr.sin_port = htons(9000);
-
-    int ret = connect(client, (struct sockaddr *)&addr, sizeof(addr));
-    if (ret == -1)
+    // Ket noi den server
+    int res = connect(client, (struct sockaddr *)&addr, sizeof(addr));
+    if (res == -1)
     {
-        perror("connect() failed");
+        printf("Khong ket noi duoc den server!");
         return 1;
     }
-char computer[64];
-char disk[2];
-short int disklength, numberdisk;
-char buf[256];
+    printf("Ket noi thanh cong den server\n");
 
 
     while (1)
     {
-          printf("Nhap ten may tinh.\n");
-          scanf("%s", computer);
+   
+
+    char buffer[256];
+    int pos = 0;
+
+    char ten_may_tinh[64];
+    printf("Nhap ten may tinh: ");
+    scanf("%s", ten_may_tinh);
+
+    int so_o_dia;
+    printf("Nhap so o dia: ");
+    scanf("%d", &so_o_dia);
+
+    stpcpy(buffer, ten_may_tinh);
+    pos = pos + strlen(ten_may_tinh);
+    buffer[pos] = 0;
+
+    getchar();
+
+    char ten_o_dia;
+    unsigned short kich_thuoc_o; 
+
+    for (int i = 0; i < so_o_dia; i++)
+    {
+        printf("Nhap ten o: ");
+        scanf("%c", &ten_o_dia);
+
+        buffer[pos] = ten_o_dia;
+        pos++;
+        printf("Nhap kich thuoc: ");
+        scanf("%hd", &kich_thuoc_o);
+        getchar();
 
 
-        if (strncmp(computer, "0000", 4) == 0)
-            break;
-
-        
+        memcpy(buffer, &kich_thuoc_o, sizeof(kich_thuoc_o));
+        pos += sizeof(kich_thuoc_o);
     }
+
+    printf("Kich thuoc buffer: %d\n", pos);
+
+    send(client, buffer, pos, 0);
     
-    
+    }
+
+    // Ket thuc, dong socket
     close(client);
+    return 0;
 }
